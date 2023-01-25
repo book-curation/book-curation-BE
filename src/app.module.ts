6d0d56add6ae513bfbcb89403bcf56ee7da957eb
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-01-25 20:58:05 +0900

    feat: 불필요한 파일삭제 및 books 모델 컨트롤러 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BooksModule } from "./books/books.module";
+import { typeORMConfig } from "./configs/typeorm.config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BooksModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
