e73438db357da3d75fe1b672bfb37da78fb8c9d7
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-02-13 19:09:11 +0900

    feat: User CRUD 작성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,72 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { Response } from 'express';
-import { UsersService } from './users.service';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  HttpStatus,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { Response } from "express";
+import { UsersService } from "./users.service";
+import { ResetPasswordDto } from "./dto/reset-password.dto";
+import { LoginDto } from "./dto/login.dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
-    constructor(private readonly usersService: UsersService){}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Post()
-    async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
-        await this.usersService.create(createUserDto)
+  @Post("login")
+  async login(@Body() loginDto: LoginDto, @Res() res: Response) {
+    await this.usersService.checkPassword(loginDto.userId, loginDto.password);
+    return res
+      .status(HttpStatus.OK)
+      .send({ message: "User successfully login" });
+  }
 
-        return res.status(200).send({ message: 'User successfully created.' })
-    }
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    await this.usersService.create(createUserDto);
+
+    return res
+      .status(HttpStatus.CREATED)
+      .send({ message: "User successfully created." });
+  }
+
+  @Get(":id")
+  async getUser(@Param("id") id: string, @Res() res: Response) {
+    const user = await this.usersService.findById(id);
+    const data = {
+      id: user.userId,
+      name: user.name,
+      status: user.status,
+      registerAt: user.registerAt,
+    };
+    return res
+      .status(HttpStatus.OK)
+      .send({ message: "User information successfully provided", data });
+  }
+
+  @Patch(":id")
+  async resetPassword(
+    @Param("id") id: string,
+    @Body() resetPasswordDto: ResetPasswordDto,
+    @Res() res: Response
+  ) {
+    await this.usersService.resetPassword(id, resetPasswordDto);
+    return res
+      .status(HttpStatus.OK)
+      .send({ message: "Password successfully reset" });
+  }
+
+  @Delete()
+  async delete(@Body() loginDto: LoginDto, @Res() res: Response) {
+    await this.usersService.delete(loginDto.userId, loginDto.password);
+    return res
+      .status(HttpStatus.OK)
+      .send({ message: "User successfully deleted" });
+  }
 }
