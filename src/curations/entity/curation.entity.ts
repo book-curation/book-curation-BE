f5149386d2d2d36dfe4cb8d4dccb23f2b1d2a424
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-02-15 19:23:38 +0900

    feat: create unit test
    Co-authored-by: keongmini <kmnam09@gmail.com>

--- src/curations/entity/curation.entity.ts
+++ src/curations/entity/curation.entity.ts
@@ -1,29 +1,36 @@
-import { Book } from "src/books/entity/book.entity";
-import { User } from "src/users/entity/user.entity";
-import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+import { Book } from "../../books/entity/book.entity";
+import { User } from "../../users/entity/user.entity";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
 
 @Entity()
 export class Curation {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    title:string;
+  @Column()
+  title: string;
 
-    @Column()
-    isPublic: boolean;
+  @Column()
+  isPublic: boolean;
 
-    @Column({type: "timestamptz"})
-    createAt: Date
+  @Column({ type: "timestamptz" })
+  createAt: Date;
 
-    @ManyToOne(() => User, (user) => user.curations)
-    user: User;
+  @ManyToOne(() => User, (user) => user.curations)
+  user: User;
 
-    @ManyToMany(() => Book)
-    @JoinTable({
-        name: "curation_booklist",
-        joinColumn: { name: "curationId" },
-        inverseJoinColumn: { name: "bookId" },
-    })
-    books: Book[];
+  @ManyToMany(() => Book)
+  @JoinTable({
+    name: "curation_booklist",
+    joinColumn: { name: "curationId" },
+    inverseJoinColumn: { name: "bookId" },
+  })
+  books: Book[];
 }
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Res } from "@nestjs/common";
+import { HttpStatus } from "@nestjs/common/enums";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Response } from "express";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
-describe('UsersController', () => {
+describe("UsersController", () => {
   let controller: UsersController;
+
+  let res: Response = {
+    status: HttpStatus.OK,
+    send:
+  }
+
+  const mockUsersService = {
+    create: jest.fn((dto) => dto),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("should create a user", async () => {
+    const response = await controller.create(
+      {
+        userId: "test@test.com",
+        password: "test123",
+        name: "test",
+      },
+      res
+    );
+    
   });
 });
