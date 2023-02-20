80bb3ae1b70958a6b79b21e74a34f13a61807219
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-02-20 18:59:56 +0900

    feat: Book API 생성

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get('recommend')
+  recommendBooks(@Query('count') count: number) {
+    return this.booksService.recommendBooks(count);
+  }
+
+  @Get(':id')
+  getBook(@Param('id') id: number) {
+    return this.booksService.findById(id);
+  }
+}
