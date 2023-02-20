0c84ea5c8da88e862fb0d5088858d3fc64754b73
keongmini <kmnam09@gmail.com>
2023-02-20 19:57:02 +0900

    test: books controller test

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
 
 describe('BooksController', () => {
   let controller: BooksController;
 
+  const testBookInfo = {
+    isbn: Date.now(),
+    title: 'test book',
+    subject: 'test number',
+    publisher: 'test piblisher',
+    author: 'test author',
+  };
+
+  const mockBooksService = {
+    findById: jest.fn(id => {
+      return {
+        id,
+        ...testBookInfo,
+      };
+    }),
+    recommendBooks: jest.fn((count = 3) => {
+      let result = [];
+      for (let i = 0; i < count; i++) {
+        result.push(i);
+      }
+      return result;
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
-    }).compile();
+      providers: [BooksService],
+    })
+      .overrideProvider(BooksService)
+      .useValue(mockBooksService)
+      .compile();
 
     controller = module.get<BooksController>(BooksController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get a list of count books', async () => {
+    const count = 5;
+    const bookList = await controller.recommendBooks(count);
+
+    expect(bookList.length).toEqual(count);
+  });
+
+  it('should get book informtaion by book id', () => {
+    const id = 100;
+    expect(controller.getBook(id)).toEqual({
+      id,
+      ...testBookInfo,
+    });
   });
 });
