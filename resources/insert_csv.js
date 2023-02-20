3b6a8909894900692d02327d38ea75926509952d
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-02-20 18:12:05 +0900

    update timeout value

--- resources/insert_csv.js
+++ resources/insert_csv.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const Pool = require("pg").Pool;
-const fastcsv = require("fast-csv");
-require("dotenv").config();
-console.log(require("dotenv").config());
+const fs = require('fs');
+const Pool = require('pg').Pool;
+const fastcsv = require('fast-csv');
+require('dotenv').config();
+console.log(require('dotenv').config());
 
-const filePath = "daejeon-sample-202212.csv";
+const filePath = 'daejeon-sample-202212.csv';
 
 let stream = fs.createReadStream(filePath);
 let csvData = [];
 let csvStream = fastcsv
   .parse()
-  .on("data", function (data) {
+  .on('data', function (data) {
     temp = [data[5], data[1], data[9], data[3], data[2]];
     csvData.push(temp);
   })
-  .on("end", function () {
+  .on('end', function () {
     // remove the first line: header
     csvData.shift();
     // create a new connection to the database
@@ -23,21 +23,25 @@ let csvStream = fastcsv
       port: 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: "curation",
+      database: 'curation',
       ssl: true,
+      min: 0,
+      max: 5,
+      acquireTimeoutMillis: 60000,
+      idleTimeoutMillis: 600000,
     });
 
     pool.connect((err, client, done) => {
       if (err) throw err;
 
       try {
-        csvData.forEach((row) => {
+        csvData.forEach(row => {
           const query = `INSERT INTO book (ISBN, title, subject, publisher, author) VALUES ($1, $2, $3, $4, $5)`;
           client.query(query, row, (err, res) => {
             if (err) {
               console.log(err.stack);
             } else {
-              console.log("inserted " + res.rowCount + " row:", row);
+              console.log('inserted ' + res.rowCount + ' row:', row);
             }
           });
         });
